670f598f0e9e70ff6eabd87d112d01accbeb3689
zainabrizvi <zainab.rizvi@ryerson.ca>
2018-02-06 02:13:34 -0500

    updated with both bots working

--- echobot.py
+++ echobot.py
@@ -1,44 +1,66 @@
-import os
-import time
-import re
+import settings
+
+import os, time, re, sys
+
+from datetime import datetime as dt
+
 from slackclient import SlackClient
 
-sclient = SlackClient(os.environ["SLACK_API_TOKEN"])
-starterbot_id = None
-MENTION_REGEX = "<.*>(.*)"
+sclient = SlackClient(os.environ["ECHOBOT_API_KEY"])
+echobot_id = 'U93SC5RA5'
+
+
+MENTION_REGEX = "<@{}>(.*)".format(echobot_id)
 
 
 def extract_message(message):
-	matches = re.search(MENTION_REGEX, message)
-	if (not (matches == None)):
-		return matches.group(1)
+    matches = re.search(MENTION_REGEX, message)
+
+    if (not (matches == None)):
+        return matches.group(1)
 
 def parse_command(information_recieved):
-	for item in information_recieved:
-		if item['type'] == "message" and not "subtype" in item:
-			message = extract_message(item['text'])
-			user = item['user']
-			channel = item['channel']
-			return message, channel, user
+    for item in information_recieved:
+        if item['type'] == "message" and not "subtype" in item:
+            message = extract_message(item['text'])
+            user = item['user']
+            channel = item['channel']
+            return message, channel, user
 
-	return None,None,None
+    return None,None,None
 
 def echo_message(message, channel, user):
-	sclient.api_call(
-		"chat.postMessage",
-		channel=channel,
-		text=message,
-		user=user
-	)
+    t = str(dt.now())[:19]
+       
+    log = """
+ Time     {}
+ Message  {}
+ Channel  {}
+ User     {}
+    
+    """.format(t, message, channel, user)
+    
+    sys.stderr.write(log)
+    
+    
+    sclient.api_call(
+        "chat.postMessage",
+        channel=channel,
+        text=message,
+        user=user
+    )
 
-if sclient.rtm_connect(with_team_state=False):
-	print ("Running")
-	while True:
-		message, channel, user = parse_command(sclient.rtm_read())
-		
-		if not message == None:
-			echo_message(message,channel,user)
 
-		time.sleep(1)
-else:
-	print ("Connection failed")
+if __name__ == "__main__":
+    
+    if sclient.rtm_connect(with_team_state=False):
+        sys.stderr.write('Echobot up n runnin\n')
+        while True:
+            message, channel, user = parse_command(sclient.rtm_read())
+
+            if not message == None:
+                echo_message(message,channel,user)
+
+                time.sleep(1)
+    else:
+        print ("Connection failed")
--- weatherbot.py
+++ weatherbot.py
@@ -1,120 +1,174 @@
+# loads .env contents
 import settings
+
+# use for approximate string matching
+import difflib
+
+import pandas as pd 
+
 import os, time, sys
-import re, difflib
+import re, json
+
+from urllib.request import urlopen
+from datetime import datetime as dt
 
 from slackclient import SlackClient
 
+keys = {
+    'weatherbot': os.environ['WEATHERBOT_API_KEY'],
+    'openweather': os.environ['OPENWEATHER_API_KEY']
+}
 
-client = SlackClient(os.environ["SLACKBOT_API_KEY"])
-starterbot_id = None
+client = SlackClient(keys['weatherbot'])
+weatherbot_id = 'U93NEAZ24'
 
-MENTION_REGEX = "<.*>(.*)"
+mention_regex = "<@{}>(.*)".format(weatherbot_id)
 
-base_url = 'https://api.openweathermap.org/data/2.5/weather?APPID={}'.format(os.environ['OPENWEATHER_API_KEY'])
+base_url = 'https://api.openweathermap.org/data/2.5/weather'
 
 # from the world cities database : https://simplemaps.com/data/world-cities
-# v. convenient
 cities = pd.read_csv('cities.csv').city
+
+# emojis assigned to each description for great fun
 emojis = {
-	'broken clouds': 'sun_behind_cloud', 
-	'clear sky': 'sun_with_face',
-	 'few clouds': 'sun_small_cloud', 
-	 'haze': 'fog', 
-	 'mist': 'fog', 
-	 'light rain': 'partly_sunny_rain', 
-	 'light snow': 'snowflake', 
-	 'moderate rain': 'umbrella_with_rain_drops', 
-	 'overcast clouds': 'cloud', 
-	 'scattered clouds': 'sun_small_cloud'
+    'broken clouds': 'sun_behind_cloud', 
+    'clear sky': 'sun_with_face',
+     'few clouds': 'sun_small_cloud', 
+     'haze': 'fog', 
+     'mist': 'fog', 
+     'light rain': 'partly_sunny_rain', 
+     'light snow': 'snowflake', 
+     'moderate rain': 'umbrella_with_rain_drops', 
+     'overcast clouds': 'cloud', 
+     'scattered clouds': 'sun_small_cloud'
 } 
 
 
 
 def get_weather(city):
-	url = '{}&q={}'.format(base_url, city)
-	res = urlopen(url)
+    """Gets the weather data for a given city"""
 
-	if res.code != 200:
-		sys.stderr.write('error: {}'.format(res.code))
-		return
+    # build the url string
+    url = '{}?APPID={}&q={}'.format(base_url,
+                                    keys['openweather'],
+                                    city.replace(' ', '+'))
+    # http get it
+    try:
+        res = urlopen(url)
+    except:
+        return {'error': 'url not found'}
+    
+    
+    if res.code != 200:
+        return {'error': 'invalid request'}
+    
+    try:
+        data = json.loads(res.read().decode('utf8'))
+    except:
+        return {'error': 'malformed data'}
 
-	data = json.loads(res.read().decode('utf8'))
-
-	return data
-
+    return data
 
 def extract_message(message):
-	matches = re.search(MENTION_REGEX, message)
-	if not (matches == None):
-		return matches.group(1)
+    """Extracts message content from a mention"""
+    
+    matches = re.search(mention_regex, message)
+    if not (matches == None):
+        return matches.group(1)
 
 def parse_command(information_recieved):
-	for item in information_recieved:
-		if item['type'] == "message" and not "subtype" in item:
-			message = extract_message(item['text'])
-			user = item['user']
-			channel = item['channel']
-			return message, channel, user
+    """Parses information from RTM and extracts command and parameters"""
+    for item in information_recieved:
+        if item['type'] == "message" and not "subtype" in item:
+            message = extract_message(item['text'])
+            user = item['user']
+            channel = item['channel']
 
-	return None,None,None
+            return message, channel, user
 
-def echo_message(message, channel, user):
-	try:
+    return None,None,None
 
-		matches = difflib.get_close_matches(message, cities)
+def handle_message(message, channel, user):
+    """Main method to handle weather data queries"""
 
-		if len(matches) == 0:
-			response = 'Can\'t even find anything close to that!'
-		else:
-			city = matches[0]
-			query = city.replace(' ', '+')
-			data = get_weather(query)
+    
+    # get the current time
+    t = str(dt.now())[:19]
 
-			desc = data['weather'][0]['description']
-			temp = int(data['main']['temp']) - 273
-			hum = data['main']['humidity']
-			vis = data['visibility']
+    # display message details
+    log = """
+ Time     {}
+ Message  {}
+ Channel  {}
+ User     {}
+    
+    """.format(t, message, channel, user)
+    
+    sys.stderr.write(log)
+    
+    
+    # check the world cities dataset for cities 
+    # whose names are approximately the given text
+    # 
+    # example: new yrk --> New York
+    matches = difflib.get_close_matches(message, cities)
+   
+    # if a city is found, grab the data for the first match
+    # from the openweather API
+    if len(matches):
+            city = matches[0]
+           
+            data = get_weather(city)
+            
+            if not 'error' in data:
+                # parse main fields
+                desc = data['weather'][0]['description']
+                
+                temp = int(data['main']['temp']) - 273   # kelvin to celsius
+                hum = data['main']['humidity']
+                vis = data['visibility']
 
-			emoji = '' if not desc in emojis else ':{}:'.format(emojis[desc])
+                # add an emoji if we've got one
+                emoji = '' if not desc in emojis else ':{}:'.format(emojis[desc])
 
-			print(desc)
-			print(desc in emojis)
+                # format the response
+                
+                header = '\n*{} Weather Report *'.format(city)
+                sig = '\nCheers, \n\t *Weatherbot*'
 
-			header = '*{} Weather Report {}*'.format(city, emoji)
+                response = '\n\t'.join([
+                        header,
+                        'Description:       {}  {}'.format(desc, emoji),
+                        'Temperature:       {}'.format(temp),
+                        'Humidity:            {}'.format(hum),
+                        'Visibility:            {}'.format(vis),
+                        sig
+                    ])
+                
+            else:
+                response = ':sob: I couldn\'t get any weather data for "{}"'.format(message)
+    else:
+        response = ':sob: I couldn\'t find any cities matching "{}"'.format(message)
 
-			sig = '\nCheers, \n\t *Weatherbot*'
 
-			response = '\n\t'.join([
-					header,
-					'Description:       {}'.format(desc),
-					'Temperature:       {}'.format(temp),
-					'Humidity:          {}'.format(hum),
-					'Visibility:        {}'.format(vis),
-					sig
-				])
+    # send the response
+    client.api_call(
+        "chat.postMessage",
+        channel=channel,
+        text=response,
+        user=user
+    )
 
-
-		
-
-		client.api_call(
-			"chat.postMessage",
-			channel=channel,
-			text=response,
-			user=user
-		)
-
-	except:
-		pass
 
 if __name__ == "__main__":
-	if client.rtm_connect(with_team_state=False):
-		print('sup')
-		while True:
-			message, channel, user = parse_command(client.rtm_read())
-			
-			if not message == None:
-				echo_message(message,channel,user)
+    if client.rtm_connect(with_team_state=False):
+        print('Weatherbot ready 2 rumbl')
+        while True:
+            message, channel, user = parse_command(client.rtm_read())
 
-			time.sleep(1)
-	else:
-		print ("Connection failed")
+            if not message == None:
+                handle_message(message,channel,user)
+            
+        time.sleep(1)
+    else:
+        print ("Connection failed")
